5c0e215711379a85ec987ed16f35b37494b66015
Mouniainada <jaganainada678@gmail.com>
2025-04-13 10:14:18 +0530

    Hello world program in springboot

---
+++ src/test/java/com/example/demo1000/Demo1000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
